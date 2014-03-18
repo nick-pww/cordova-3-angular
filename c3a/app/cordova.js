ec4bfb186166a30086d52b5b4ae893a41d320c52
Nick Stuart <nick@portlandwebworks.com>
2014-03-18 14:20:24 -0400

    Cordova-fy the application.

---
+++ c3a/app/cordova.js
@@ -0,0 +1,1 @@
+/** Empty place holder replaced by Cordova CLI **/
---
+++ c3a/app/scripts/CordovaInit.js
@@ -0,0 +1,33 @@
+
+'use strict';
+
+var CordovaInit = function() {
+
+	var onDeviceReady = function() {
+		receivedEvent('deviceready');
+	};
+
+	var receivedEvent = function(event) {
+		console.log('Start event received, bootstrapping application setup.');
+		angular.bootstrap($('body'), ['c3aApp']);
+	};
+	
+	this.bindEvents = function() {
+		document.addEventListener('deviceready', onDeviceReady, false);
+	};
+
+	//If cordova is present, wait for it to initialize, otherwise just try to
+	//bootstrap the application.
+	if (window.cordova !== undefined) {
+		console.log('Cordova found, wating for device.');
+		this.bindEvents();
+	} else {
+		console.log('Cordova not found, booting application');
+		receivedEvent('manual')
+	}
+};
+
+$(function() {
+	console.log('Bootstrapping!');
+	new CordovaInit();
+});
